5181dc818d5dbf38fec004665afd8967393f8388
javierBelloch <jbellochmolinero@gmail.com>
2024-05-15 18:19:04 +0200

    primeros cambios
    sala y sesion

--- CodigoAuxiliar/src/cine/Sala.java
+++ CodigoAuxiliar/src/cine/Sala.java
@@ -1,5 +1,70 @@
 package cine;
+import java.util.Arrays;
 
 public class Sala {
+    private String pelicula;
+    private String[] horasSesiones;
+    private int filas;
+    private int columnas;
+    private Sesion[] sesiones;
 
+    public Sala(String tituloPelicula, String[] horasSesiones, int filas, int columnas) {
+        this.pelicula = tituloPelicula;
+        this.horasSesiones = horasSesiones;
+        this.filas = filas;
+        this.columnas = columnas;
+        this.sesiones = new Sesion[horasSesiones.length];
+        for (int i = 0; i < horasSesiones.length; i++) {
+            sesiones[i] = new Sesion(filas, columnas);
+        }
+    }
+    public ButacasContiguas recomendarButacasContiguas1(int numButacas, int sesion) {
+        if (sesion > 0 && sesion <= sesiones.length) {
+            return sesiones[sesion - 1].recomendarButacasContiguas(numButacas);
+        }
+        return null;
+    }
+    public String getPelicula() {
+        return pelicula;
+    }
+
+    public void comprarEntrada(int sesion, int fila, int columna) {
+        sesiones[sesion - 1].comprarEntrada(fila, columna);
+    }
+
+    public int getIdEntrada(int sesion, int fila, int columna) {
+        return sesiones[sesion - 1].getIdEntrada(fila, columna);
+    }
+
+    public String[] getHorasDeSesionesDeSala() {
+        return horasSesiones;
+    }
+
+    public int[][] getEstadoSesion(int sesion) {
+        return sesiones[sesion - 1].getEstado();
+    }
+
+    public int getButacasDisponiblesSesion(int sesion) {
+        return sesiones[sesion - 1].getButacasDisponibles();
+    }
+
+    public String recogerEntradas(int idVenta, int sesion) {
+        if (sesion > 0 && sesion <= sesiones.length) {
+            return sesiones[sesion - 1].recogerEntradas(idVenta);
+        }
+        return null;
+    }
+
+    public ButacasContiguas recomendarButacasContiguas(int numButacas, int sesion) {
+        if (sesion > 0 && sesion <= sesiones.length) {
+            return sesiones[sesion - 1].recomendarButacasContiguas(numButacas);
+        }
+        return null;
+    }
+
+    public void comprarEntradasRecomendadas(ButacasContiguas butacas, int sesion) {
+        if (sesion > 0 && sesion <= sesiones.length && butacas != null) {
+            sesiones[sesion - 1].comprarEntradasRecomendadas(butacas);
+        }
+    }
 }
--- CodigoAuxiliar/src/cine/Sesion.java
+++ CodigoAuxiliar/src/cine/Sesion.java
@@ -1,5 +1,72 @@
 package cine;
 
 public class Sesion {
+    private String hora;
+    private int[][] estadoAsientos;
+    private int asientosDisponibles;
+    private int sigIdCompra;
 
+    public Sesion(int filas, int columnas) {
+        this.estadoAsientos = new int[filas][columnas];
+        this.asientosDisponibles = filas * columnas;
+        this.sigIdCompra = 1;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public int[][] getEstado() {
+        return estadoAsientos;
+    }
+
+    public int getButacasDisponibles() {
+        return asientosDisponibles;
+    }
+
+    public void comprarEntrada(int fila, int columna) {
+        if (estadoAsientos[fila - 1][columna - 1] == 0) {
+            estadoAsientos[fila - 1][columna - 1] = sigIdCompra;
+            sigIdCompra++;
+            asientosDisponibles--;
+        }
+    }
+
+    public int getIdEntrada(int fila, int columna) {
+        return estadoAsientos[fila - 1][columna - 1];
+    }
+
+    public String recogerEntradas(int idVenta) {
+        StringBuilder entradas = new StringBuilder();
+        boolean encontrada = false;
+        for (int i = 0; i < estadoAsientos.length; i++) {
+            for (int j = 0; j < estadoAsientos[i].length; j++) {
+                if (estadoAsientos[i][j] == idVenta) {
+                    encontrada = true;
+                    entradas.append("--").append(i + 1).append(",").append(j + 1);
+                    estadoAsientos[i][j] = 0;
+                    asientosDisponibles++;
+                }
+            }
+        }
+        return encontrada ? entradas.toString().substring(2) : null;
+    }
+
+    public ButacasContiguas recomendarButacasContiguas(int numButacas) {
+        // Implementación de la recomendación de butacas contiguas
+        // Aquí debes implementar la lógica para recomendar las butacas contiguas
+        // Puedes usar cualquier algoritmo que prefieras para este propósito
+        // Retornarías un objeto de tipo ButacasContiguas con la fila y columna recomendadas
+        return null;
+    }
+
+    public void comprarEntradasRecomendadas(ButacasContiguas butacas) {
+        // Implementación para comprar las butacas recomendadas
+        // Aquí deberías marcar las butacas como compradas en el estadoAsientos
+        // y actualizar el número de asientos disponibles
+    }
 }
